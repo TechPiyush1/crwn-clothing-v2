db763f0ba1678b94e07a1d6c1b7ca51b1f7adb2b
Pradhuman Singh <pradhumansingh871@gmail.com>
2023-07-12 10:10:29 +0530

    Added a sign in component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 
 import { Routes,Route } from "react-router-dom";
+import Authorize from "./routes/sign-in/sign-in.component";
 
 const Shop=()=>
 {
@@ -18,6 +19,7 @@ function App() {
       <Route path="/" element={<Navigation/>}>
       <Route index element={<Home/>}/>
       <Route path="shop" element={<Shop/>}/>
+      <Route path="sign" element={<Authorize/>}/>
       </Route> 
     </Routes>
    
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,11 @@
+
+
+function Authorize() {
+  return (
+    <div>
+      <h1>Welcome and Sign-in</h1>
+    </div>
+  )
+}
+
+export default Authorize
